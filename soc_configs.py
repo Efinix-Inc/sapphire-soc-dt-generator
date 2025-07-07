f8d0bd7da84f3ca8942d95a140a673a3bd7a5421
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2025-07-07 10:49:38 +0800

    soc_configs.py: add method to get cpu mmu
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- soc_configs.py
+++ soc_configs.py
@@ -1,9 +1,10 @@
 from controller import Controller
 
 class SocConfigs(Controller):
-    def __init__(self, configs):
+    def __init__(self, configs, arch=32):
         super().__init__(configs, dev_type="soc")
         self.configs = configs
+        self.arch = arch
 
     def get_frequency(self):
         """Return the peripherals frequency"""
@@ -16,6 +17,15 @@ class SocConfigs(Controller):
     def get_cpu_count(self):
         """Return number of cpu cores"""
         return self.configs.get("cpus", {}).get("cores", 0)
+
+    def get_cpu_mmu_type(self):
+        """Return the RISCV mmu type"""
+        if self.arch == 64:
+            return "riscv,sv39"
+        elif self.arch == 32:
+            return "riscv,sv32"
+        else:
+            return ""
 
     def get_peripherals_configs(self):
         """Return the peripherals configurations"""
