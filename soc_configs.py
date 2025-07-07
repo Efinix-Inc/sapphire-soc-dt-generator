478d38a91b9eaf9377788db600091815e19dbad2
Mohamad Noor Alim Hussin <mnalim@efinixinc.com>
2025-07-07 10:23:09 +0800

    soc_configs.py: add more methods to retrive soc configurations
    Signed-off-by: Mohamad Noor Alim Hussin <mnalim@efinixinc.com>

--- soc_configs.py
+++ soc_configs.py
@@ -6,7 +6,17 @@ class SocConfigs(Controller):
         self.configs = configs
 
     def get_frequency(self):
+        """Return the peripherals frequency"""
         return self.configs.get("frequency", 0)
 
     def get_cpu_isa(self):
+        """Return the cpu RISCV ISA"""
         return self.configs.get("cpus", {}).get("isa", "")
+
+    def get_cpu_count(self):
+        """Return number of cpu cores"""
+        return self.configs.get("cpus", {}).get("cores", 0)
+
+    def get_peripherals_configs(self):
+        """Return the peripherals configurations"""
+        return self.configs.get("peripherals", {})
